3531b98bdb521afe5eca7a81cf8552c447ee5d59
mont266 <scottmontford@gmail.com>
2022-07-27 00:14:24 +0100

    Update 0.410
    -Netlify forms added to contact page

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -18,7 +18,7 @@ const ContactForm = () => {
                     </div>
 
                     <div className="col-lg-6 col-md-12">
-                        <form id="contactForm">
+                        <form id="contactForm" name="contact" netlify>
                             <div className="row">
                                 <div className="col-lg-12 col-md-12">
                                     <div className="form-group">
